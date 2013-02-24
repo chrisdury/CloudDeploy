0c2333ac2cd3a5052c88868a0ab45d4fca18ae63
Chris Dury <chrisdury@gmail.com>
2013-02-24 10:50:32 +1030

    WIP - managing deployment units

--- CloudDeploy/WebUI/Controllers/ReleaseController.cs
+++ CloudDeploy/WebUI/Controllers/ReleaseController.cs
@@ -7,43 +7,69 @@ using System.Web;
 using System.Web.Mvc;
 using CloudDeploy.Model.Releases;
 using CloudDeploy.Persistence.Contexts;
+using CloudDeploy.Model.Platform;
 
 namespace WebUI.Controllers
 {
     public class ReleaseController : Controller
     {
         private ReleaseContext db = new ReleaseContext();
-
+        const string ViewDataKeys_Artefacts = "Artefacts";
+        const string ViewDataKeys_Builds = "Builds";
 
 
         public ActionResult ManageDeploymentUnits(Guid id)
         {
             ReleasePackage releasepackage = db.ReleasePackages.Find(id);
-
-            
-            
-
             return View(releasepackage);
         }
 
         public ActionResult AddDeploymentUnit(Guid id)
         {
             ReleasePackage releasepackage = db.ReleasePackages.Find(id);
-            ViewData.Add("Artefacts", db.GetDeployableArtefacts().ToList().Select(da => new SelectListItem() { Text = da.DeployableArtefactName, Value = da.DeployableArtefactID.ToString() }).AsEnumerable<SelectListItem>());
+            ViewData.Add(ViewDataKeys_Artefacts, db.GetDeployableArtefacts().ToList().Select(da => new SelectListItem() { Text = da.DeployableArtefactName, Value = da.DeployableArtefactID.ToString() }).AsEnumerable<SelectListItem>());
+            ViewData.Add(ViewDataKeys_Builds, db.GetBuilds().ToList().Select(b => new SelectListItem() { Text = b.BuildName, Value = b.BuildLabel }).AsEnumerable<SelectListItem>());
+
             return View(releasepackage);
         }
 
         [HttpPost]
-        public ActionResult AddDeploymentUnit(FormCollection fc)
+        public ActionResult AddDeploymentUnit(Guid id, FormCollection fc)
         {
-            //ReleasePackage releasepackage = db.ReleasePackages.Find(id);
+            ReleasePackage releasepackage = db.ReleasePackages.Find(id);
 
-            
-
-            RedirectToAction("ManageDeploymentUnits");
+            if (fc[ViewDataKeys_Artefacts] != null && fc[ViewDataKeys_Artefacts].Length > 0 && fc[ViewDataKeys_Builds] != null && fc[ViewDataKeys_Builds].Length > 0)
+            {
+                var artefactsIDs = fc[ViewDataKeys_Artefacts].Split(',');
+                var build = db.GetBuildByLabel(fc[ViewDataKeys_Builds]);
+                foreach (var artefactID in artefactsIDs)
+                {
+                    var artefaceGuid = new Guid(artefactID);
+                    releasepackage.AddArtefactToPackage(
+                        db.GetDeployableArtefacts().Single(da => da.DeployableArtefactID == artefaceGuid),
+                        build);
+                }
+                db.SaveChanges();
+                return RedirectToAction("ManageDeploymentUnits", new { id = id });
+            }            
             return View();
         }
 
+        public ActionResult RemoveDeploymentUnit(Guid id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RemoveDeploymentUnit(Guid id, Guid deploymentUnitId)
+        {
+            var package = db.GetReleasePackages().Single(rp => rp.ReleasePackageID == id);
+            var deploymentUnit = db.DeploymentUnits.Single(du => du.DeploymentUnitID == deploymentUnitId);
+            package.RemoveArtefactFromPackage(deploymentUnit.DeployableArtefact);
+            db.SaveChanges();
+            return RedirectToAction("ManageDeploymentUnits", new { id = id });
+            //return View();
+        }
 
 
 
